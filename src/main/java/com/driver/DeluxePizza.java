0e406fd7f63f7e6ba7b0ec65af75bb5b64b18102
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2023-05-23 15:47:52 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,6 +5,6 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        super.e
+        super.addExtraToppings();
     }
 }
